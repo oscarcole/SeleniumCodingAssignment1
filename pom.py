e6370e8634a04ab815ff55a8f3da289f3a6e0960
oscarcole <33353992+oscarcole@users.noreply.github.com>
2021-04-11 17:15:19 -0600

    Added in stubbs

--- pom.py
+++ pom.py
@@ -1,27 +1,45 @@
+# This is a test for verifying that properties listed belong to the agent selected on Zoopla.co.uk
+
+class VerifySeller:
+    def __init__(self):
+        pass
+
 # Given that the page https://www.zoopla.co.uk/ is visible
 # TODO
 
 # Then the search location contains 'London'
 # TODO
+    def city_text(self):
+        pass
 
 # And the search button is clicked
 # TODO
+    def click_search(self):
+        pass
 
 # Then the price values are printed in descending order in the console
 # TODO
+    def price_values(self):
+        pass
 
-# And the 5th property on that list is selected
+# And the 5th property on that list is selected (properties constantly updated, might not be the same)
 # TODO
+    def select_fifth(self):
 
-# Then the 5th property on that list is clicked
-# TODO
+        # Then the 5th property on that list is clicked
+        # TODO
+        pass
 
 # Then the agent name is stored
 # TODO
+    def agent_info(self):
+        agent_name = ''
 
-# Then the 'View agent properties' button is clicked
-# TODO
+        # Then the 'View agent properties' button is clicked
+        # TODO
+        pass
 
 # Then assert that all properties on that page belong to the agent selected
 # TODO
-
+    def properties_belong_list(self):
+        pass
