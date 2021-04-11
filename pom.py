d3d4a867d3ecf24514565675424d95655c3e0779
oscarcole <33353992+oscarcole@users.noreply.github.com>
2021-04-11 16:37:35 -0600

    Added in TODO list with Gherkin

---
+++ pom.py
@@ -0,0 +1,27 @@
+# Given that the page https://www.zoopla.co.uk/ is visible
+# TODO
+
+# Then the search location contains 'London'
+# TODO
+
+# And the search button is clicked
+# TODO
+
+# Then the price values are printed in descending order in the console
+# TODO
+
+# And the 5th property on that list is selected
+# TODO
+
+# Then the 5th property on that list is clicked
+# TODO
+
+# Then the agent name is stored
+# TODO
+
+# Then the 'View agent properties' button is clicked
+# TODO
+
+# Then assert that all properties on that page belong to the agent selected
+# TODO
+
