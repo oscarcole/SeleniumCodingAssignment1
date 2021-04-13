c3e3b2b3f4bb78100f85bbc002e617f6c3cb28bb
oscarcole <33353992+oscarcole@users.noreply.github.com>
2021-04-12 22:59:00 -0600

    Added in 'go' function, fleshed out accept_cookies, location_text, click_search, and added the test at bottom of page.

--- pom.py
+++ pom.py
@@ -1,37 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
 # This is a test for verifying that properties listed belong to the agent selected on Zoopla.co.uk
+browser = webdriver.Chrome()
+
 
 class VerifySeller:
-    def __init__(self):
-        pass
+    def __init__(self, driver):
+        # Given that the page https://www.zoopla.co.uk/ is visible
+        self.driver = driver
+        self.url = 'https://www.zoopla.co.uk/'
 
-# Given that the page https://www.zoopla.co.uk/ is visible
-# TODO
+    def go(self):
+        self.driver.get(self.url)
 
-# Then the search location contains 'London'
-# TODO
-    def city_text(self):
-        pass
+    # Alert present requesting cookie preferences || target element in list >
+    # ui-button-primary ui-cookie-accept-all-medium-large || div name for list >
+    # $$("div[class = 'ui-cookie-consent-choose__buttons']")
+    def accept_cookies(self):
+        accept_button = self.driver.find_element_by_css_selector('button.'
+                                                                 'ui-button-primary.'
+                                                                 'ui-cookie-accept-all-medium-large')
+        accept_button.click()
+        return None
 
-# And the search button is clicked
-# TODO
+    # Then the search location contains 'London' || $$("input[id = 'search-input-location-wrapper']")
+    def location_text(self, text):
+        inpt_txt = self.driver.find_element_by_id('search-input-location-wrapper')
+        element_txt = inpt_txt.get_attribute('value')
+        return element_txt
+
+    # And the search button is clicked || $$("button[id = 'search-submit']")
+    # TODO
     def click_search(self):
-        pass
+        search_button = self.driver.find_element_by_id('search-submit')
+        search_button.click()
+        return None
 
-# Then the price values are printed in descending order in the console
-# TODO
+    # Then the price values are printed in descending order in the console
+    # TODO
     def price_values(self):
         pass
 
-# And the 5th property on that list is selected (properties constantly updated, might not be the same)
-# TODO
+    # And the 5th property on that list is selected (properties constantly updated, might not be the same)
+    # TODO
     def select_fifth(self):
-
         # Then the 5th property on that list is clicked
         # TODO
         pass
 
-# Then the agent name is stored
-# TODO
+    # Then the agent name is stored
+    # TODO
+
     def agent_info(self):
         agent_name = ''
 
@@ -39,7 +60,19 @@ class VerifySeller:
         # TODO
         pass
 
-# Then assert that all properties on that page belong to the agent selected
-# TODO
+    # Then assert that all properties on that page belong to the agent selected
+    # TODO
     def properties_belong_list(self):
         pass
+
+
+# ============================
+# Test
+# ============================
+
+
+browser = webdriver.Chrome()
+testing_page = VerifySeller(driver=browser)
+testing_page.go()
+testing_page.accept_cookies()
+# browser.quit()
