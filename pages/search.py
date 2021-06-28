558c2b5daece68b9eb24db9bc28e0be2d72e7c45
oscarcole <33353992+oscarcole@users.noreply.github.com>
2021-06-27 20:52:34 -0600

    Added in search.py, result.py, & test_search.py. Inserted TODO's, started to structure out each page. Still in a little disarray, but will fix.

---
+++ pages/search.py
@@ -0,0 +1,35 @@
+
+# GIVEN the URL "https://www.zoopla.co.uk/" is displayed
+URL = 'https://www.zoopla.co.uk/'
+
+# Locators
+COOKIES = ''
+SEARCH_INPUT = ''
+DRP_BTN = ''
+
+
+# AND the accept cookies pop up is clicked
+# TODO
+
+# THEN the search input contains "London"
+# TODO
+
+# WHEN the search button is clicked
+# TODO
+
+# WHEN the dropdown button is selected, choosing "lowest price"
+# TODO
+
+# WHEN the 5th property on the list is clicked
+# TODO
+
+# AND the agent's name in listing card is stored
+# TODO
+
+# AND the "View Agent Properties" button is clicked
+# TODO
+
+# AND the "Agents name in the listing card is stored
+# TODO
+
+
---
+++ tests/test_search.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Assert that the property agent matches the card
+# TODO
+
+# TODO: Remove exception once test is compelete
+raise Exception("Test is not completed.")
